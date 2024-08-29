e8795ca60c2cfc3a5bf3b8db4a3dd5ce3cac8cab
Michael Rios <riosmichael2895@gmail.com>
2024-08-29 12:29:43 -0700

    Update wezterm conf

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,11 +7,14 @@ config.use_fancy_tab_bar = false
 config.show_tabs_in_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.enable_tab_bar = false
-config.window_decorations = 'RESIZE'
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 config.disable_default_key_bindings = true
 
+config.initial_cols = 180
+config.initial_rows = 40
+
 config.window_padding = {
-    top = '1cell'
+    top = '2cell'
 }
 
 return config
