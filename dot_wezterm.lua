c9f0031c21eaa4e14af88deb46229b2fce4c7d12
Michael Rios <riosmichael2895@gmail.com>
2024-11-18 16:46:19 -0800

    Update

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 9.0
+config.font_size = 11.0
 config.color_scheme = 'Dark+'
 config.use_fancy_tab_bar = false
 config.show_tabs_in_tab_bar = false
